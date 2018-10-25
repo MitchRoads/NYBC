388cd1b97c46fbc27d889ce156e2566cbe04de5e
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-24 23:13:20 -0400

    Update nybc.js

--- nybc.js
+++ nybc.js
@@ -89,11 +89,11 @@ message.channel.send(helpEmbed);
 }
 	
 	if (message.content.toLowerCase().startsWith(`${prefix}remind`)) {
-		message.channel.guild.members.forEach(user => {
+		message.channel.guild.members.every(user => {
 if(!message.member.hasPermission("MANAGE_MESSAGES"))
 return message.channel.send("You don't have the permissions to manage messages, you will not be able to do this command.");
 let args = message.content.split(/ +/g).slice(1)
-let remindtime = args[0];
+let remindtime = args[0]; 
 if(!remindtime) return message.channel.send("You didn't put a time!");
 setTimeout(function(){
 let botmessage = args.slice(1).join(" ");
