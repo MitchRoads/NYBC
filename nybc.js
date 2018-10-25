ff4eabde847db1c0e5525d2fadb06f958d232059
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-24 21:35:39 -0400

    Update nybc.js

--- nybc.js
+++ nybc.js
@@ -101,7 +101,7 @@ let botmessage = args.join(" ");
   let testembed = new Discord.RichEmbed()
   .setColor("#2B547E")
   .setDescription(`${botmessage}`)
-message.members.send(testembed)
+message.guild.members.send(testembed)
 }, ms(remindtime)); 
 }
 	
