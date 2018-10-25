c538d7ed0089ebdbf00af499d67596e7cf8c81d9
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-25 12:43:34 -0400

    Update nybc.js

--- nybc.js
+++ nybc.js
@@ -95,7 +95,7 @@ return message.channel.send("You don't have the permissions to manage messages, 
 let args = message.content.split(/ +/g).slice(1)
 let remindtime = args[0]; 
 if (!remindtime)
-return message.channel.send("You didn't put a time!");
+message.channel.send("You didn't put a time!");
 setTimeout(function(){
 let botmessage = args.slice(1).join(" ");
   let testembed = new Discord.RichEmbed()
