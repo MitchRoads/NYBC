aae14d1db016f02d3bf93a9ce2bffe58ec5a41e6
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-24 22:52:27 -0400

    Update nybc.js

--- nybc.js
+++ nybc.js
@@ -89,13 +89,13 @@ message.channel.send(helpEmbed);
 }
 	
 	if (message.content.toLowerCase().startsWith(`${prefix}remind`)) {
+		message.channel.guild.members.forEach(user => {
 if(!message.member.hasPermission("MANAGE_MESSAGES"))
 return message.channel.send("You don't have the permissions to manage messages, you will not be able to do this command.");
 let args = message.content.split(/ +/g).slice(1)
 let remindtime = args[0];
 if(!remindtime) return message.channel.send("You didn't put a time!");
 setTimeout(function(){
-message.channel.guild.members.forEach(user => {
 let botmessage = args[1];
   let testembed = new Discord.RichEmbed()
   .setColor("#2B547E")
